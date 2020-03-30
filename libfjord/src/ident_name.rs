78c80fb44b38a7652d96432bfa595024603b6af2
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-03-30 22:46:33 +1100

    Add tests for IdentName behaviour

--- libfjord/src/ident_name.rs
+++ libfjord/src/ident_name.rs
@@ -11,3 +11,39 @@ impl IdentName {
         Ok((s, Self(name.into())))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn camel_case() {
+        assert_eq!(
+            IdentName::new("camelCase"),
+            Ok(("", IdentName("camelCase".into())))
+        )
+    }
+
+    #[test]
+    fn pascal_case() {
+        assert!(IdentName::new("PascalCase").is_err())
+    }
+
+    #[test]
+    fn shouty_snake_case() {
+        assert!(IdentName::new("SHOUTY_SNAKE_CASE").is_err())
+    }
+
+    // These two tests assert not that the cases fail, but that they don’t use up all of their
+    // input.
+
+    #[test]
+    fn snake_case() {
+        assert_ne!(IdentName::new("kebab-case").unwrap().0, "");
+    }
+
+    #[test]
+    fn kebab_case() {
+        assert_ne!(IdentName::new("kebab-case").unwrap().0, "");
+    }
+}
