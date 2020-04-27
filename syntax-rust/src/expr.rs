a70b1292fe0c8f8b8a6ae84404cdf1aa6cdb9638
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-04-27 11:29:39 +1000

    [syntax-rust] Add support for character literals

--- syntax-rust/src/expr.rs
+++ syntax-rust/src/expr.rs
@@ -1,10 +1,18 @@
-use nom::bytes::complete::{tag, take_until};
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take, take_until},
+};
 
 // TODO: Implement more expression types.
 // HACK: Rust mistakenly doesn’t realise that the variants of this enum are actually used.
 #[allow(dead_code)]
 #[derive(Debug, PartialEq)]
 pub(crate) enum Expr<'text> {
+    Character {
+        start_quote: &'text str,
+        contents: &'text str,
+        end_quote: &'text str,
+    },
     String {
         start_quote: &'text str,
         contents: &'text str,
@@ -14,6 +22,25 @@ pub(crate) enum Expr<'text> {
 
 impl<'text> Expr<'text> {
     pub(crate) fn new(s: &'text str) -> nom::IResult<&'text str, Self> {
+        alt((Self::new_character, Self::new_string))(s)
+    }
+
+    fn new_character(s: &'text str) -> nom::IResult<&'text str, Self> {
+        let (s, start_quote) = tag("'")(s)?;
+        let (s, contents) = take(1usize)(s)?;
+        let (s, end_quote) = tag("'")(s)?;
+
+        Ok((
+            s,
+            Self::Character {
+                start_quote,
+                contents,
+                end_quote,
+            },
+        ))
+    }
+
+    fn new_string(s: &'text str) -> nom::IResult<&'text str, Self> {
         let (s, start_quote) = tag("\"")(s)?;
         let (s, contents) = take_until("\"")(s)?;
         let (s, end_quote) = tag("\"")(s)?;
@@ -32,6 +59,24 @@ impl<'text> Expr<'text> {
 impl<'e> From<Expr<'e>> for Vec<syntax::HighlightedSpan<'e>> {
     fn from(expr: Expr<'e>) -> Self {
         match expr {
+            Expr::Character {
+                start_quote,
+                contents,
+                end_quote,
+            } => vec![
+                syntax::HighlightedSpan {
+                    text: start_quote,
+                    group: Some(syntax::HighlightGroup::CharacterDelimiter),
+                },
+                syntax::HighlightedSpan {
+                    text: contents,
+                    group: Some(syntax::HighlightGroup::Character),
+                },
+                syntax::HighlightedSpan {
+                    text: end_quote,
+                    group: Some(syntax::HighlightGroup::CharacterDelimiter),
+                },
+            ],
             Expr::String {
                 start_quote,
                 contents,
