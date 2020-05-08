0112ceb512fdad4d97f8ba9aae2c2fd604bfc574
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-05-08 20:06:33 +1000

    [syntax-rust] Enfore matching delimiter in macro invocations

--- syntax-rust/src/parser.rs
+++ syntax-rust/src/parser.rs
@@ -969,7 +969,13 @@ fn function_call(s: &str) -> ParseResult<'_> {
 
 fn macro_invocation(s: &str) -> ParseResult<'_> {
     let open_delim = alt((tag("("), tag("["), tag("{")));
-    let close_delim = alt((tag(")"), tag("]"), tag("}")));
+
+    let convert_open_delim_to_close = |s| match s {
+        "(" => ")",
+        "[" => "]",
+        "{" => "}",
+        _ => unreachable!(),
+    };
 
     let (s, path) = path(s)?;
     let (s, name) = snake_case(s)?;
@@ -979,12 +985,15 @@ fn macro_invocation(s: &str) -> ParseResult<'_> {
     let (s, open_delim) = open_delim(s)?;
     let (s, open_delim_space) = take_whitespace0(s)?;
 
+    // This is the delimiter we’re looking for to close the macro invocation.
+    let close_delim = convert_open_delim_to_close(open_delim);
+
     // This isn’t 100% correct, as macros can take any valid token tree as input. However, it’s
     // close enough for most macros.
-    let (s, mut params) = comma_separated(&|s| alt((item, statement))(s), "")(s)?;
+    let (s, mut params) = comma_separated(&|s| alt((item, statement))(s), close_delim)(s)?;
     let (s, params_space) = take_whitespace0(s)?;
 
-    let (s, close_delim) = close_delim(s)?;
+    let (s, close_delim) = tag(close_delim)(s)?;
 
     let mut output = path;
 
