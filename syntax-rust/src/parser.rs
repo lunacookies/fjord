a2de2eefbf539868fe421cf57f4fa10d97bba45d
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-05-08 13:52:39 +1000

    [syntax-rust] Add support for trait declarations

--- syntax-rust/src/parser.rs
+++ syntax-rust/src/parser.rs
@@ -114,7 +114,7 @@ pub(crate) fn parse(s: &str) -> ParseResult<'_> {
 }
 
 fn item(s: &str) -> ParseResult<'_> {
-    alt((use_, ty_alias, function, structure_def))(s)
+    alt((trait_, use_, ty_alias, function, structure_def))(s)
 }
 
 fn whitespace(s: &str) -> ParseResult<'_> {
@@ -151,6 +151,75 @@ fn error_1_char(s: &str) -> ParseResult<'_> {
             group: Some(syntax::HighlightGroup::Error),
         }]
     })(s)
+}
+
+fn trait_(s: &str) -> ParseResult<'_> {
+    let (s, keyword) = tag("trait")(s)?;
+    let (s, keyword_space) = take_whitespace1(s)?;
+
+    let (s, name) = pascal_case(s)?;
+    let (s, name_space) = take_whitespace0(s)?;
+
+    let (s, open_brace) = tag("{")(s)?;
+    let (s, open_brace_space) = take_whitespace0(s)?;
+
+    let (s, items) = many0(|s| {
+        let (s, item) = item(s)?;
+        let (s, space) = take_whitespace0(s)?;
+
+        let mut output = item;
+        output.push(syntax::HighlightedSpan {
+            text: space,
+            group: None,
+        });
+
+        Ok((s, output))
+    })(s)?;
+
+    let (s, close_brace_space) = take_whitespace0(s)?;
+    let (s, close_brace) = tag("}")(s)?;
+
+    let mut output = vec![
+        syntax::HighlightedSpan {
+            text: keyword,
+            group: Some(syntax::HighlightGroup::OtherKeyword),
+        },
+        syntax::HighlightedSpan {
+            text: keyword_space,
+            group: None,
+        },
+        syntax::HighlightedSpan {
+            text: name,
+            group: Some(syntax::HighlightGroup::InterfaceDef),
+        },
+        syntax::HighlightedSpan {
+            text: name_space,
+            group: None,
+        },
+        syntax::HighlightedSpan {
+            text: open_brace,
+            group: Some(syntax::HighlightGroup::Delimiter),
+        },
+        syntax::HighlightedSpan {
+            text: open_brace_space,
+            group: None,
+        },
+    ];
+
+    output.append(&mut items.concat());
+
+    output.extend_from_slice(&[
+        syntax::HighlightedSpan {
+            text: close_brace_space,
+            group: None,
+        },
+        syntax::HighlightedSpan {
+            text: close_brace,
+            group: Some(syntax::HighlightGroup::Delimiter),
+        },
+    ]);
+
+    Ok((s, output))
 }
 
 fn use_(s: &str) -> ParseResult<'_> {
