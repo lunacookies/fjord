859365f2e3f28974dfd905c3cecc6a4e3d48bc54
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-05-08 14:40:27 +1000

    [syntax] Remove SpecialIdentSigil variants

--- syntax/src/lib.rs
+++ syntax/src/lib.rs
@@ -66,10 +66,6 @@ pub enum HighlightGroup {
     SpecialIdentDef,
     /// the name of a special identifier when used
     SpecialIdentUse,
-    /// a sigil used to indicate a special identifier when the identifer is being defined
-    SpecialIdentDefSigil,
-    /// a sigil used to indicate a special identifier when the identifer is being used
-    SpecialIdentUseSigil,
     /// the name of a function parameter
     FunctionParam,
     /// a number literal (whether integer or floating-point)
