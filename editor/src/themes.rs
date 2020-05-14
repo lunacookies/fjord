4f3b904a61be46f4d3c50ddb39745b50b4be68c3
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-05-14 11:13:31 +1000

    [editor] Add Seoul theme

--- editor/src/themes.rs
+++ editor/src/themes.rs
@@ -1,11 +1,13 @@
 mod dark_plus;
 mod gruvbox;
 mod monokai;
+mod seoul;
 mod solarized;
 
 pub(crate) use {
     dark_plus::DarkPlus,
     gruvbox::Gruvbox,
     monokai::Monokai,
+    seoul::Seoul,
     solarized::{SolarizedDark, SolarizedLight},
 };
