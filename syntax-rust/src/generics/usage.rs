8a95e418f22e445c9692936feb1dcbdbeb12d51b
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-06-02 08:46:12 +1000

    Remove crates that have been migrated into separate repos

--- syntax-rust/src/generics/usage.rs
+++
@@ -1,9 +0,0 @@
-use {crate::ParseResult, nom::branch::alt};
-
-pub(crate) fn parse(s: &str) -> ParseResult<'_> {
-    super::parse(&param)(s)
-}
-
-fn param(s: &str) -> ParseResult<'_> {
-    alt((crate::lifetime::usage, crate::ty))(s)
-}
