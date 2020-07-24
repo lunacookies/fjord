50f4f20340f0db847dad184a04a2622084daf1c0
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-07-24 22:14:45 +1000

    Fix bug where Atom could contain dollar sign

--- src/lexer/syntax_kind.rs
+++ src/lexer/syntax_kind.rs
@@ -4,7 +4,7 @@ use num_enum::{IntoPrimitive, TryFromPrimitive};
 #[derive(Debug, Logos, Copy, Clone, PartialEq, IntoPrimitive, TryFromPrimitive)]
 #[repr(u16)]
 pub(crate) enum SyntaxKind {
-    #[regex(r"([^\n ]|\\ )+")]
+    #[regex(r"([^\n $]|\\ )+")]
     Atom,
 
     #[regex("[0-9]+", priority = 2)]
@@ -82,6 +82,16 @@ mod tests {
     #[test]
     fn lex_dollar_sign() {
         test("$", SyntaxKind::Dollar);
+    }
+
+    #[test]
+    fn lex_dollar_sign_then_atom() {
+        let mut lexer = SyntaxKind::lexer("$foo");
+
+        assert_eq!(lexer.next(), Some(SyntaxKind::Dollar));
+        assert_eq!(lexer.slice(), "$");
+        assert_eq!(lexer.next(), Some(SyntaxKind::Atom));
+        assert_eq!(lexer.slice(), "foo");
     }
 
     #[test]
