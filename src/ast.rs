330a344d18e02e91b46577f439e990b953a203d5
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-07-25 23:32:27 +1000

    Create AST node for Item

--- src/ast.rs
+++ src/ast.rs
@@ -21,6 +21,8 @@ macro_rules! ast_node {
 
 ast_node!(Root, SyntaxKind::Root);
 
+ast_node!(Item, SyntaxKind::Item);
+
 ast_node!(Expr, SyntaxKind::Expr);
 
 ast_node!(Statement, SyntaxKind::Statement);
