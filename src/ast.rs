2f26747336fc479e68e9cf53375729bf972a8ae3
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-07-25 23:07:43 +1000

    Add initial basic AST

---
+++ src/ast.rs
@@ -0,0 +1,36 @@
+use crate::lexer::SyntaxKind;
+use crate::SyntaxNode;
+
+macro_rules! ast_node {
+    ($node:ident, $kind:expr) => {
+        #[allow(unused)]
+        struct $node(SyntaxNode);
+
+        impl $node {
+            #[allow(unused)]
+            fn cast(node: SyntaxNode) -> Option<Self> {
+                if node.kind() == $kind {
+                    Some(Self(node))
+                } else {
+                    None
+                }
+            }
+        }
+    };
+}
+
+ast_node!(Root, SyntaxKind::Root);
+
+ast_node!(Expr, SyntaxKind::Expr);
+
+ast_node!(Statement, SyntaxKind::Statement);
+
+ast_node!(BindingDef, SyntaxKind::BindingDef);
+
+ast_node!(FunctionCall, SyntaxKind::FunctionCall);
+
+ast_node!(FunctionCallParams, SyntaxKind::FunctionCallParams);
+
+ast_node!(Lambda, SyntaxKind::Lambda);
+
+ast_node!(LambdaParams, SyntaxKind::LambdaParams);
