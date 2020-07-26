14bbd695d40a83858c0978454c6c72568f085861
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-07-26 22:39:59 +1000

    Allow creating child Envs

--- src/env.rs
+++ src/env.rs
@@ -2,11 +2,19 @@ use crate::val::Val;
 use smol_str::SmolStr;
 use std::collections::HashMap;
 
-pub(crate) struct Env {
+pub(crate) struct Env<'parent> {
     bindings: HashMap<SmolStr, Val>,
+    parent: Option<&'parent Self>,
 }
 
-impl Env {
+impl<'parent> Env<'parent> {
+    pub(crate) fn create_child(&'parent self) -> Self {
+        Self {
+            bindings: HashMap::new(),
+            parent: Some(self),
+        }
+    }
+
     pub(crate) fn store_binding(&mut self, name: SmolStr, val: Val) {
         self.bindings.insert(name, val);
     }
