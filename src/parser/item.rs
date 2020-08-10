0fcb2008427235dc95016a9ad967ea18f9a0d71a
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-08-10 13:28:03 +1000

    Fix bug where did not parse return statement in parse_item

--- src/parser/item.rs
+++ src/parser/item.rs
@@ -4,10 +4,9 @@ use super::Parser;
 use crate::lexer::SyntaxKind;
 
 pub(crate) fn parse_item(p: &mut Parser<'_>) {
-    if p.peek() == Some(SyntaxKind::Let) {
-        parse_statement(p);
-    } else {
-        parse_expr(p);
+    match p.peek() {
+        Some(SyntaxKind::Let) | Some(SyntaxKind::Return) => parse_statement(p),
+        _ => parse_expr(p),
     }
 }
 
@@ -30,7 +29,7 @@ Root@0..1
     }
 
     #[test]
-    fn parse_statement() {
+    fn parse_binding_def() {
         test(
             "let x = $y",
             r#"
@@ -45,6 +44,21 @@ Root@0..10
     BindingUsage@8..10
       Dollar@8..9 "$"
       Atom@9..10 "y""#,
+        );
+    }
+
+    #[test]
+    fn parse_return_statement() {
+        test(
+            "return $x",
+            r#"
+Root@0..9
+  ReturnStatement@0..9
+    Return@0..6 "return"
+    Whitespace@6..7 " "
+    BindingUsage@7..9
+      Dollar@7..8 "$"
+      Atom@8..9 "x""#,
         );
     }
 }
